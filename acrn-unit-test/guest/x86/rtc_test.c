5b469ea48f23a6065443847143e97d5574a724df
liu,qiang <qiangx.j.liu@intel.com>
2019-12-18 03:16:44 -0500

    Modify the code format

--- acrn-unit-test/guest/x86/rtc_test.c
+++ acrn-unit-test/guest/x86/rtc_test.c
@@ -1,13 +1,8 @@
 /*
- *
  *  file rtc_test.c
- *
  */
-
-
 #include "libcflat.h"
 #include "x86/asm/io.h"
-
 
 #define RTC_INDEX_REG		0x70
 #define RTC_TARGET_REG		0x71
@@ -26,7 +21,6 @@
 #define C_INDEX            	0xC
 #define B_DEFAULT_VALUE 	0x6
 
-
 static uint8_t read_reg_al()
 {
 	uint8_t al;
@@ -38,14 +32,14 @@ static uint8_t read_reg_al()
 	return al;
 }
 
-
 /**
- *  @brief Case name: When a vCPU attempts to write guest RTC target register, ACRN hypervisor shall ignore the write_001
+ *  @brief Case name: When a vCPU attempts to write guest RTC target register,
+ *  ACRN hypervisor shall ignore the write_001
  *
- * Summary: the value of the RTC target register has no change after writing this target register with different data
+ * Summary: the value of the RTC target register has no change after writing this target
+ * register with different data
  *
  */
-
 void rtc_rqmid_24557_write_guest_rtc_target_register(void)
 {
 	uint8_t reg8_value_prev;
@@ -60,15 +54,13 @@ void rtc_rqmid_24557_write_guest_rtc_target_register(void)
 	report("\t\t rtc_rqmid_24557_write_guest_rtc_target_register", reg8_value == reg8_value_prev);
 }
 
-
 /**
- * @brief Case name: RTC_When a vCPU attempts to read guest RTC target register and the current guest RTC index is greater than BH,
- *  ACRN hypervisor shall write 0 to guest AL_001
+ * @brief Case name: RTC_When a vCPU attempts to read guest RTC target register and the current guest 
+ * RTC index is greater than BH, ACRN hypervisor shall write 0 to guest AL_001
  *
  * Summary: dump the value of AL after reading from RTC target register
  *
  */
-
 void rtc_rqmid_24494_reading_rtc_target_register(void)
 {
 	uint8_t reg8_value;
@@ -80,15 +72,14 @@ void rtc_rqmid_24494_reading_rtc_target_register(void)
 	report("\t\t rtc_rqmid_24494_reading_rtc_target_register", reg8_value > 0xB);
 }
 
-
-
 /**
- * @brief Case name: RTC When a vCPU attempts to write 2 bytes to guest RTC index register, ACRN hypervisor shall ignore the write_001
+ * @brief Case name: RTC When a vCPU attempts to write 2 bytes to guest RTC index register,
+ * ACRN hypervisor shall ignore the write_001
  *
- * Summary: RTC When a vCPU attempts to write 2 bytes to guest RTC index register, ACRN hypervisor shall ignore the write_001
+ * Summary: RTC When a vCPU attempts to write 2 bytes to guest RTC index register,
+ * ACRN hypervisor shall ignore the write_001
  *
  */
-
 void rtc_rqmid_24560_write_2_bytes_to_guest_rtc_index_register(void)
 {
 	uint16_t reg16_value;
@@ -100,14 +91,13 @@ void rtc_rqmid_24560_write_2_bytes_to_guest_rtc_index_register(void)
 	report("\t\t rtc_rqmid_24560_write_2_bytes_to_guest_rtc_index_register", reg16_value == B_INDEX);
 }
 
-
 /**
- * @brief Case name: RTC_When a vCPU attempts to read guest RTC target register and the current guest RTC index is BH, ACRN hypervisor shall write 6H to guest AL_001
+ * @brief Case name: RTC_When a vCPU attempts to read guest RTC target register and the current guest
+ * RTC index is BH, ACRN hypervisor shall write 6H to guest AL_001
  *
  * Summary: dump the value of AL after reading from RTC target register
  *
  */
-
 void rtc_rqmid_24495_reading_rtc_target_register_and_current_guest_rtc_index_is_BH(void)
 {
 	uint8_t reg_b_value;
@@ -116,17 +106,17 @@ void rtc_rqmid_24495_reading_rtc_target_register_and_current_guest_rtc_index_is_
 	(void)inb(RTC_TARGET_REG);
 	reg_b_value = read_reg_al();
 
-	report("\t\t rtc_rqmid_24495_reading_rtc_target_register_and_current_guest_rtc_index_is_BH %d", reg_b_value == B_DEFAULT_VALUE,reg_b_value);
+	report("\t\t rtc_rqmid_24495_reading_rtc_target_register_and_current_guest_rtc_index_is_BH %d",
+		reg_b_value == B_DEFAULT_VALUE,reg_b_value);
 }
 
-
 /**
- * @brief Case name: When a vCPU attempts to write guest RTC index register, ACRN hypervisor shall keep guest AL as the current guest RTC index_001
+ * @brief Case name: When a vCPU attempts to write guest RTC index register,
+ * ACRN hypervisor shall keep guest AL as the current guest RTC index_001
  *
  * Summary:  the value of AL is keeped to be current RTC index register after writing guest RTC index register
  *
  */
-
 void rtc_rqmid_24556_reading_rtc_AL_is_keeped_to_be_current_rtc_index_register(void)
 {
 	uint8_t reg8_value;
@@ -134,17 +124,17 @@ void rtc_rqmid_24556_reading_rtc_AL_is_keeped_to_be_current_rtc_index_register(v
 	outb(YEAR_INDEX, RTC_INDEX_REG);
 	reg8_value = read_reg_al();
 
-	report("\t\t rtc_rqmid_24556_reading_rtc_AL_is_keeped_to_be_current_rtc_index_register %d", reg8_value == YEAR_INDEX, reg8_value);
+	report("\t\t rtc_rqmid_24556_reading_rtc_AL_is_keeped_to_be_current_rtc_index_register %d",
+		reg8_value == YEAR_INDEX, reg8_value);
 }
 
-
 /**
- * @brief Case name: When a vCPU attempts to write 2 bytes to guest RTC target register, ACRN hypervisor shall ignore the write_001
+ * @brief Case name: When a vCPU attempts to write 2 bytes to guest RTC target register,
+ * ACRN hypervisor shall ignore the write_001
  *
  * Summary:  the value of RTC target register has no change after writing 2 bytes to this target register
  *
  */
-
 void rtc_rqmid_24558_writing_2bytes_to_target_register_register_has_no_change(void)
 {
 	uint16_t reg16_value_prev;
@@ -155,17 +145,17 @@ void rtc_rqmid_24558_writing_2bytes_to_target_register_register_has_no_change(vo
 	outw(0xFFFF, RTC_TARGET_REG);
 	reg16_value = inw(RTC_TARGET_REG);
 
-	report("\t\t rtc_rqmid_24558_writing_2bytes_to_target_register_register_has_no_change %d", reg16_value == reg16_value_prev, reg16_value);
+	report("\t\t rtc_rqmid_24558_writing_2bytes_to_target_register_register_has_no_change %d",
+		reg16_value == reg16_value_prev, reg16_value);
 }
 
-
 /**
- * @brief Case name: when a vCPU attempts to read guest RTC index register, ACRN hypervisor shall write 0H to guest AL_001
+ * @brief Case name: when a vCPU attempts to read guest RTC index register,
+ * ACRN hypervisor shall write 0H to guest AL_001
  *
  * Summary:  the value of AL should be always 0H even if RTC index register is set to a different value
  *
  */
-
 void rtc_rqmid_24387_writing_write_0h_to_guest_al(void)
 {
 	uint8_t reg8_value;
@@ -177,35 +167,33 @@ void rtc_rqmid_24387_writing_write_0h_to_guest_al(void)
 	report("\t\t rtc_rqmid_24387_writing_write_0h_to_guest_al %d", reg8_value == 0, reg8_value);
 }
 
-
 static void print_case_list(void)
 {
 	printf("\t\t RTC feature case list:\n\r");
-
 	printf("\t\t Segmentation 64-Bits Mode:\n\r");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24557u, "When a vCPU attempts to write guest RTC target register, ACRN hypervisor shall ignore the write_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24494u, "RTC When a vCPU attempts to read guest RTC target register and the current guest RTC index is greater than BH, ACRN hypervisor shall write 0 to guest AL_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24560u, "RTC When a vCPU attempts to write 2 bytes to guest RTC index register, ACRN hypervisor shall ignore the write_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24495u, "RTC_When a vCPU attempts to read guest RTC target register and the current guest RTC index is BH, ACRN hypervisor shall write 6H to guest AL_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24556u, "When a vCPU attempts to write guest RTC index register, ACRN hypervisor shall keep guest AL as the current guest RTC index_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24558u, "When a vCPU attempts to write 2 bytes to guest RTC target register, ACRN hypervisor shall ignore the write_001");
-
-	printf("\t\t Case ID:%d case name:%s\n\r", 24387u, "when a vCPU attempts to read guest RTC index register, ACRN hypervisor shall write 0H to guest AL_001");
-
+	printf("\t\t Case ID:%d case name:%s\n\r", 24557u,
+		"When a vCPU attempts to write guest RTC target register, ACRN hypervisor shall ignore the write_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24494u,
+		"RTC When a vCPU attempts to read guest RTC target register and the current guest RTC index is greater"
+		" than BH, ACRN hypervisor shall write 0 to guest AL_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24560u,
+		"RTC When a vCPU attempts to write 2 bytes to guest RTC index register,"
+		" ACRN hypervisor shall ignore the write_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24495u,
+		"RTC_When a vCPU attempts to read guest RTC target register and the current guest RTC index is BH,"
+		"ACRN hypervisor shall write 6H to guest AL_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24556u,
+		"When a vCPU attempts to write guest RTC index register, ACRN hypervisor shall keep guest"
+		"AL as the current guest RTC index_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24558u,
+		"When a vCPU attempts to write 2 bytes to guest RTC target register,"
+		"ACRN hypervisor shall ignore the write_001");
+	printf("\t\t Case ID:%d case name:%s\n\r", 24387u,
+		"when a vCPU attempts to read guest RTC index register, ACRN hypervisor shall write 0H to guest AL_001");
 }
-
-
-
 
 static void test_rtc(void)
 {
-
 	rtc_rqmid_24557_write_guest_rtc_target_register();
 	rtc_rqmid_24494_reading_rtc_target_register();
 	rtc_rqmid_24560_write_2_bytes_to_guest_rtc_index_register();
@@ -213,17 +201,11 @@ static void test_rtc(void)
 	rtc_rqmid_24556_reading_rtc_AL_is_keeped_to_be_current_rtc_index_register();
 	rtc_rqmid_24558_writing_2bytes_to_target_register_register_has_no_change();
 	rtc_rqmid_24387_writing_write_0h_to_guest_al();
-
 }
-
-
 
 int main(void)
 {
 	print_case_list();
-
 	test_rtc();
-
 	return report_summary();
 }
-
