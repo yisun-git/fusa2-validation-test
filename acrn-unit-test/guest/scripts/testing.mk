b193895f891ea1134ae4585387e114d22086cc15
Jiang, Yanting <yanting.jiang@intel.com>
2020-02-26 13:56:07 +0800

    Fix error in coding guidlines scans
    Signed-off-by: Jiang, Yanting <yanting.jiang@intel.com>

--- acrn-unit-test/guest/scripts/testing.mk
+++ acrn-unit-test/guest/scripts/testing.mk
@@ -31,7 +31,7 @@ codescan: $(C_CODESCAN)
                 if [ $$next -gt 0 ]; then \
                         echo "$$line" >> $(TMP_SCAN_FINAL); \
                         next=$$((next-1)); \
-                elif [[ $$rst -eq 1 && "$$line" =~ "warning:" && ! "$$line" = lib/x86/* ]]; then \
+                elif [[ $$rst -eq 1 && "$$line" =~ "warning:" && ! "$$line" = lib/* ]]; then \
                         echo $$line >> $(TMP_SCAN_FINAL); \
                         next=2; \
                 fi \
