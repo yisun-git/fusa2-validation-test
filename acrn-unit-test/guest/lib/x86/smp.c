39c4f818bedfce6f6a2300cc8bd6d22ad6097b5c
liu,qiang <qiangx.j.liu@intel.com>
2020-07-01 02:04:44 -0400

    for clang check coding style

--- acrn-unit-test/guest/lib/x86/smp.c
+++ acrn-unit-test/guest/lib/x86/smp.c
@@ -21,115 +21,120 @@ static atomic_t active_cpus;
 
 static __attribute__((used)) void ipi(void)
 {
-    void (*function)(void *data) = ipi_function;
-    void *data = ipi_data;
-    bool wait = ipi_wait;
+	void (*function)(void *data) = ipi_function;
+	void *data = ipi_data;
+	bool wait = ipi_wait;
 
-    if (!wait) {
-	ipi_done = 1;
-	apic_write(APIC_EOI, 0);
-    }
-    function(data);
-    atomic_dec(&active_cpus);
-    if (wait) {
-	ipi_done = 1;
-	apic_write(APIC_EOI, 0);
-    }
+	if (!wait) {
+		ipi_done = 1;
+		apic_write(APIC_EOI, 0);
+	}
+	function(data);
+	atomic_dec(&active_cpus);
+	if (wait) {
+		ipi_done = 1;
+		apic_write(APIC_EOI, 0);
+	}
 }
 
 asm (
-     "ipi_entry: \n"
-     "   call ipi \n"
+	"ipi_entry: \n"
+	"   call ipi \n"
 #ifndef __x86_64__
-     "   iret"
+	"   iret"
 #else
-     "   iretq"
+	"   iretq"
 #endif
-     );
+	);
 
 int cpu_count(void)
 {
-    return _cpu_count;
+	return _cpu_count;
 }
 
 /* Because the first page is used for AP startup,
  * save the SMP ID to the second page
  */
-#define SMP_ID_ADDR 	0x1000
+#define SMP_ID_ADDR	0x1000
 
 int smp_id(void)
 {
-    unsigned id;
+	unsigned id;
 
-    asm ("mov %%gs:"xstr(SMP_ID_ADDR)", %0" : "=r"(id));
-    return id;
+	asm ("mov %%gs:"xstr(SMP_ID_ADDR)", %0" : "=r"(id));
+	return id;
 }
 
 static void setup_smp_id(void *data)
 {
-    asm ("mov %0, %%gs:"xstr(SMP_ID_ADDR)"" : : "r"(apic_id()) : "memory");
+	asm ("mov %0, %%gs:"xstr(SMP_ID_ADDR)"" : : "r"(apic_id()) : "memory");
 }
 
 static void __on_cpu(int cpu, void (*function)(void *data), void *data,
-                     int wait)
+		     int wait)
 {
-    spin_lock(&ipi_lock);
-    if (cpu == smp_id())
-	function(data);
-    else {
-	atomic_inc(&active_cpus);
-	ipi_done = 0;
-	ipi_function = function;
-	ipi_data = data;
-	ipi_wait = wait;
-	apic_icr_write(APIC_INT_ASSERT | APIC_DEST_PHYSICAL | APIC_DM_FIXED
-                       | IPI_VECTOR,
-                       cpu);
-	while (!ipi_done)
-	    ;
-    }
-    spin_unlock(&ipi_lock);
+	spin_lock(&ipi_lock);
+	if (cpu == smp_id()) {
+		function(data);
+	}
+	else {
+		atomic_inc(&active_cpus);
+		ipi_done = 0;
+		ipi_function = function;
+		ipi_data = data;
+		ipi_wait = wait;
+		apic_icr_write(APIC_INT_ASSERT | APIC_DEST_PHYSICAL | APIC_DM_FIXED
+			       | IPI_VECTOR,
+			       cpu);
+		while (!ipi_done) {
+			;
+		}
+	}
+	spin_unlock(&ipi_lock);
 }
 
 void on_cpu(int cpu, void (*function)(void *data), void *data)
 {
-    __on_cpu(cpu, function, data, 1);
+	__on_cpu(cpu, function, data, 1);
 }
 
 void on_cpu_async(int cpu, void (*function)(void *data), void *data)
 {
-    __on_cpu(cpu, function, data, 0);
+	__on_cpu(cpu, function, data, 0);
 }
 
 void on_cpus(void (*function)(void *data), void *data)
 {
-    int cpu;
+	int cpu;
 
-    for (cpu = cpu_count() - 1; cpu >= 0; --cpu)
-        on_cpu_async(cpu, function, data);
+	for (cpu = cpu_count() - 1; cpu >= 0; --cpu) {
+		on_cpu_async(cpu, function, data);
+	}
 
-    while (cpus_active() > 1)
-        pause();
+	while (cpus_active() > 1) {
+		pause();
+	}
 }
 
 int cpus_active(void)
 {
-    return atomic_read(&active_cpus);
+	return atomic_read(&active_cpus);
 }
 
 void smp_init(void)
 {
-    int i;
-    void ipi_entry(void);
+	int i;
+	void ipi_entry(void);
 
-    _cpu_count = fwcfg_get_nb_cpus();
+	_cpu_count = fwcfg_get_nb_cpus();
 
-    setup_idt();
-    set_idt_entry(IPI_VECTOR, ipi_entry, 0);
+	setup_idt();
+	set_idt_entry(IPI_VECTOR, ipi_entry, 0);
 
-    setup_smp_id(0);
-    for (i = 1; i < cpu_count(); ++i)
-        on_cpu(i, setup_smp_id, 0);
+	setup_smp_id(0);
+	for (i = 1; i < cpu_count(); ++i) {
+		on_cpu(i, setup_smp_id, 0);
+	}
 
-    atomic_inc(&active_cpus);
+	atomic_inc(&active_cpus);
 }
