fb4f96ab00c1a4b65640365ff5e6701a20b35b49
qianouyang <qian.ouyang@intel.com>
2024-04-18 09:49:02 +0800

    merge commit e76a8543c3969b25ed5e5614fb5689c5e4417e41

--- acrn-unit-test/guest/scripts/modify_lds.sh
+++ acrn-unit-test/guest/scripts/modify_lds.sh
@@ -7,3 +7,5 @@ start_address="    . = $@;"
 sed -i "3c${start_address}"  x86/flat.lds
 sed -i "3s/^/    /" x86/flat.lds
 
+sed -i "26c${start_address}" x86/flat_hsi.lds
+sed -i "26s/^/    /" x86/flat_hsi.lds
